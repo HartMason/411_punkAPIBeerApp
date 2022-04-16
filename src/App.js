655c83b1d42beb190d4cc6b0290bf212504b16c2
HartMason <hartmason85@gmail.com>
2022-04-15 22:20:22 -0500

    started styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import BeerCard from "./BeerCard";
 
-function App() {
+const App = () => {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://api.punkapi.com/v2/beers").then((response) => {
+      setdata(response.data);
+    });
+  }, []);
+
+  // useEffect(()=>{
+  //   console.log(data)
+  // }, [data])
+
+  const handleClick = (button) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-    
+        <header id="header">Punk API Beer APP</header>
+        <p id="beerList">Beer List</p>
+      <section className="grid">
+        {/* if we want to run javascript code inside of our return
+      we need to wrap it in curly braces */}
+        {data.map((beer) => {
+          return <BeerCard key={beer.id} beer={beer} />;
+        })}
+
+      </section>
+        <button id="button" onClick={handleClick}>
+          button
+        </button>
     </div>
   );
-}
-//testing if push was successful 
+};
 export default App;
---
+++ src/BeerCard.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./BCard.css"; 
+
+export default function BeerCard(props) {
+  // const {beer} = props
+  const beer = props.beer;
+  const [isLiked, setIsLiked] = useState(false);
+  const handleLike = (e, id) => {
+    setIsLiked(!isLiked);
+    console.log(isLiked);
+  };
+
+  return (
+    <div>
+      <p className="beerName">{beer.name}</p>
+      <p>{beer.abv}</p>
+      <img
+        src={beer.image_url}
+        className="beerPic"
+        alt={beer.name}
+        style={{ height: "100px" }}
+      />
+      <br/>
+      {isLiked ? (
+        <button onClick={(e) => handleLike(e, beer.id)} className="like">
+          UnLike
+        </button>
+      ) : (
+        <button onClick={(e) => handleLike(e, beer.id)} className="like">
+          Like
+        </button>
+      )}
+    </div>
+  );
+}
